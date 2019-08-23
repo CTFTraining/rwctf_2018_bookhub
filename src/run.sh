8ad46c086de0bc7a2f902477685f931d2908c930
glzjin <glzjin@zhaojin97.cn>
2019-08-23 08:16:00 +0800

    Allow dynamic flag

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+echo $FLAG > /flag_378d0265d46a10d2f6d99ea199fb39d4
+export FLAG=not_flag
+FLAG=not_flag
+
 redis-server &
 
 exec gunicorn -w 4 -b :8000 -u nobody -g nogroup --access-logfile - app:app
